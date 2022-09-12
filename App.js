1afd4b7d2fdf153a5713e84378e9ff1d569543c6
erik <erik_g@web.de>
2022-09-12 22:17:50 +0300

    Assignment 4 complete

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, FlatList, Text, SafeAreaView} from 'react-native';
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+
+  const buttonHandlerAdd = () => {
+    setData([...data, { key: text }]);
+
+  }
+
+  const buttonHandlerClear = () => {
+    setData([]);
+    
+  }
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View>
+
+        <TextInput style={styles.textInput} onChangeText={text => setText(text)} />
+
+      </View>
+
+      <View style={styles.div}>
+
+        <Button title='ADD' onPress={buttonHandlerAdd}>
+
+        </Button>
+
+        <Button title='CLEAR' onPress={buttonHandlerClear}>
+
+        </Button>
+
+      </View>
+
+      <View>
+
+        <Text style={styles.text}>
+          Shopping List
+        </Text>
+
+        <SafeAreaView style={styles.list}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) => <Text>{item.key}</Text>}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </SafeAreaView>
+
+      </View>
+
     </View>
   );
 }
@@ -16,5 +64,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 200
   },
+  textInput: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 70
+  },
+  div: {
+    flexDirection: 'row',
+    marginBottom: 30
+
+  },
+  text: {
+    color: '#0000ff',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  list: {
+    alignItems: 'center'
+  }
 });
